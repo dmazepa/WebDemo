b5a9eccba53c52da33a94ffa79881a7c3a127b73
dmazepa <dmazepa@speroteck.com>
2022-01-13 11:57:24 +0200

    edit foo.py10

--- foo.py
+++ foo.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-
+import os
+os.chmod('./chromedriver', 0755)
 ser = Service('./chromedriver')
 op = webdriver.ChromeOptions()
 op.add_argument("window-size=1920x1480")
