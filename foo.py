601361f2d3e76ca8ecb0cc7d483e342dc6798f2f
dmazepa <dmazepa@speroteck.com>
2022-01-13 12:25:49 +0200

    edit foo.py13

--- foo.py
+++ foo.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+import chromedriver_binary
 import os
 os.chmod('./chromedriver', 0o755)
 path = os.getenv('PATH');
