240be7d7334fa47feb497dbb65b2eb2ba2ce8c0e
dmazepa <dmazepa@speroteck.com>
2022-01-13 10:56:21 +0200

    edit foo.py

--- foo.py
+++ foo.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 gChromeOptions = webdriver.ChromeOptions()
 gChromeOptions.add_argument("window-size=1920x1480")
 gChromeOptions.add_argument("disable-dev-shm-usage")
-gDriver = webdriver.Chrome(chrome_options=gChromeOptions)
+gDriver = webdriver.Chrome()
 gDriver.get("https://www.python.org/")
 time.sleep(3)
 gDriver.save_screenshot("my_screenshot.png")
